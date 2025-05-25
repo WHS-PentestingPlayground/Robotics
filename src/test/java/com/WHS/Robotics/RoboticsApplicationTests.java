0d2baac1af409621f50c53a3035e31564b940af0
MEspeaker <sho3492@gmail.com>
2025-05-25 22:03:20 +0900

    [CREATE]setting

---
+++ src/test/java/com/WHS/Robotics/RoboticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WHS.Robotics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

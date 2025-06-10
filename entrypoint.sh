e64d0ba86f06c37cde65ccf27f3a996f54d96fee
meowyeok <vkdnjaldur@gmail.com>
2025-06-11 01:12:12 +0900

    [FIX] 도커 실행 오류

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@ catalina.sh run &
 
 # readflag 파일이 생길 때까지 대기 후 실행 권한 부여
 while [ ! -f /usr/local/tomcat/webapps/ROOT/readflag ]; do
-  sleep 1
+ sleep 1
 done
 chmod +x /usr/local/tomcat/webapps/ROOT/readflag
 
 # Tomcat 프로세스가 끝날 때까지 대기
-wait 
+wait
